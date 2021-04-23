44615ebfc909fed26fe8bcd205f55987709f86a0
Hieu Le <hieuld1009@gmail.com>
2021-04-23 18:30:23 +0700

    .

--- SocialMediaServer/Startup.cs
+++ SocialMediaServer/Startup.cs
@@ -28,6 +28,9 @@ namespace SocialMediaServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddDefaultPolicy(
+                builder => builder.AllowAnyOrigin()
+            ));
             services.AddSingleton<MemoryDataAndEmailService>();
             services.AddSingleton<EncryptionAndCompressService>();
             services.AddSingleton<IMongoClient, MongoClient>(s => {
